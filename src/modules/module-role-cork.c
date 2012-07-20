70d0bcf104d8f9da1494ecd7069f306e3cf4577a
Sjoerd Simons <sjoerd@debian.org>
2012-07-20 19:42:57 +0200

    Imported Upstream version 2.1

--- src/modules/module-role-cork.c
+++ src/modules/module-role-cork.c
@@ -225,9 +225,12 @@ int pa__init(pa_module *m) {
     if (roles) {
         const char *split_state = NULL;
         char *n = NULL;
-        while ((n = pa_split(roles, ",", &split_state)))
+        while ((n = pa_split(roles, ",", &split_state))) {
             if (n[0] != '\0')
-                pa_idxset_put(u->trigger_roles, pa_xstrdup(n), NULL);
+                pa_idxset_put(u->trigger_roles, n, NULL);
+            else
+                pa_xfree(n);
+        }
     }
     if (pa_idxset_isempty(u->trigger_roles)) {
         pa_log_debug("Using role 'phone' as trigger role.");
@@ -239,9 +242,12 @@ int pa__init(pa_module *m) {
     if (roles) {
         const char *split_state = NULL;
         char *n = NULL;
-        while ((n = pa_split(roles, ",", &split_state)))
+        while ((n = pa_split(roles, ",", &split_state))) {
             if (n[0] != '\0')
-                pa_idxset_put(u->cork_roles, pa_xstrdup(n), NULL);
+                pa_idxset_put(u->cork_roles, n, NULL);
+            else
+                pa_xfree(n);
+        }
     }
     if (pa_idxset_isempty(u->cork_roles)) {
         pa_log_debug("Using roles 'music' and 'video' as cork roles.");
@@ -289,7 +295,7 @@ void pa__done(pa_module *m) {
             pa_xfree(role);
         pa_idxset_free(u->trigger_roles, NULL, NULL);
     }
-    if (u->trigger_roles) {
+    if (u->cork_roles) {
         while ((role = pa_idxset_steal_first(u->cork_roles, NULL)))
             pa_xfree(role);
         pa_idxset_free(u->cork_roles, NULL, NULL);
